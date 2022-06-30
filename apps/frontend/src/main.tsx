055061d4dee1c7e4818ca771a697e294a1f42331
Matt Young <mattyoung305@gmail.com>
2022-07-01 01:26:44 +0200

    Fixing base eslint config

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,6 +1,5 @@
-import * as ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom';
 
 import App from './app/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
